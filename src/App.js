09847214010434c620e3acc57f9199b7b07c2c6d
erikgervas <erikgervas@gmail.com>
2022-08-22 00:04:47 -0300

    react-memo-example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './wdyr';
 import './App.css';
+import {useState, memo, useMemo} from "react";
 
-function App() {
+const getInitialItems = () => [
+    { text: 'Item 1', price: 20 },
+    { text: 'Item 2', price: 30 },
+    { text: 'Item 3', price: 10 }
+];
+
+const List = memo(({ items }) => {
   return (
-    <div className="App">
-    </div>
+      <div>
+      { items
+          .map((item, key) =>
+              <div key={key}>
+                  item: {item.text}
+              </div>)
+      }
+      </div>)
+});
+
+List.whyDidYouRender = true;
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [items, setItems] = useState(getInitialItems(10));
+
+    return (
+      <div className="App">
+        <div className="Box">
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>
+                inc
+            </button>
+            <List items={items} />
+        </div>
+      </div>
   );
 }
+
+App.whyDidYouRender = true;
 
 export default App;
