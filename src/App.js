88658b3e135e47813f8cb32a5ce3b90b0fe18a65
erikgervas <erikgervas@gmail.com>
2022-09-06 17:34:34 -0300

    useMemo factorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './wdyr';
 import './App.css';
 
+import {useMemo, useState} from 'react';
+
+export function MyComponent() {
+  const [number, setNumber] = useState(1);
+  const [inc, setInc] = useState(0);
+
+  const factorialResult = useMemo(() => calculateFactorial(number) , [number]);
+  const onChange = event => {
+    setNumber(Number(event.target.value));
+  };
+  const onClick = () => setInc(i => i + 1);
+
+  return (
+      <>
+        Factorial of the number
+        <input type="number" value={number} onChange={onChange} />
+        is {factorialResult}
+        <button onClick={onClick}>Increment</button> <span>{inc}</span>
+      </>
+  );
+}
+
+function calculateFactorial(number) {
+  console.log('calculateFactorial called!');
+  return number <= 0 ? 1 : number * calculateFactorial(number - 1);
+}
+
 function App() {
   return (
     <div className="App">
+      <MyComponent/>
     </div>
   );
 }
